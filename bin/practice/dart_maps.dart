f34395b1a0406c83ce79918b223ea3340f3d5e2b
KENIL VASOYA <131660623+Candy117@users.noreply.github.com>
2024-08-19 19:35:53 +0530

    Maps Push

--- bin/practice/dart_maps.dart
+++ bin/practice/dart_maps.dart
@@ -10,7 +10,15 @@ void main() {
   Map<int, int> intData = {1: 6, 11: 2};
   // String and integer
   Map<String, int> bothData = {"Id": 1, "SubId": 8};
+  // dynamic = we can store both value String and int
+  Map<String, dynamic> data = {
+    "Id" : 1,
+    "Name" : "Hasti",
+    "Age" : 20,
+    "Surname" : "Vasoya"
+  };
   print(myData);
   print(intData);
   print(bothData);
+  print(data);
 }
