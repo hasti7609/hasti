eed73c3ba37abaa77b65c7113d0dad6114130cd7
KENIL VASOYA <131660623+Candy117@users.noreply.github.com>
2024-08-19 22:33:58 +0530

    Map Values

--- bin/practice/dart_maps.dart
+++ bin/practice/dart_maps.dart
@@ -1,24 +1,29 @@
 // String pass
 
 void main() {
-  Map<String, String> myData = {
-    "Name": "Hasti",
-    "Surname": "Vasoya",
-    "PartnerName": "Parag"
-  };
-  //integer
-  Map<int, int> intData = {1: 6, 11: 2};
-  // String and integer
-  Map<String, int> bothData = {"Id": 1, "SubId": 8};
-  // dynamic = we can store both value String and int
-  Map<String, dynamic> data = {
-    "Id" : 1,
-    "Name" : "Hasti",
-    "Age" : 20,
-    "Surname" : "Vasoya"
-  };
-  print(myData);
-  print(intData);
-  print(bothData);
-  print(data);
+  // Map<String, String> myData = {
+  //   "Name": "Hasti",
+  //   "Surname": "Vasoya",
+  //   "PartnerName": "Parag"
+  // };
+  // //integer
+  // Map<int, int> intData = {1: 6, 11: 2};
+  // // String and integer
+  // Map<String, int> bothData = {"Id": 1, "SubId": 8};
+  // // dynamic = we can store both value String and int
+  // Map<String, dynamic> data = {
+  //   "Id" : 1,
+  //   "Name" : "Hasti",
+  //   "Age" : 20,
+  //   "Surname" : "Vasoya"
+  // };
+  // print(myData);
+  // print(intData);
+  // print(bothData);
+  // print(data);
+  Map<String, dynamic> myMapData = {"Id": 1, "Name": "Hasti"};
+  myMapData["City"] = "Surat";
+  //print(myMapData.keys);
+  //print(myMapData.values);
+  print(myMapData.isNotEmpty);
 }
