dda3e0ca244050a6abbcb85eb00158239a618808
KENIL VASOYA <131660623+Candy117@users.noreply.github.com>
2024-08-19 12:21:52 +0530

    Commit_new

---
+++ bin/new_dart_project.dart
@@ -0,0 +1,5 @@
+import 'package:new_dart_project/new_dart_project.dart' as new_dart_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${new_dart_project.calculate()}!');
+}
---
+++ test/new_dart_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:new_dart_project/new_dart_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
